756a3cc2634e4c9a6edc4c79738a3041c1bf3f40
clement bonnemaire <clement.bonnemaire@gmail.com>
2018-02-19 18:00:34 +0800

    completed the MVC structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'json'
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+list_of_ingredients = JSON.parse(user_serialized)
+
+
+
+
+puts "Deleting all ingredients"
+Ingredient.destroy_all
+puts "Creating ingredients"
+
+
+36.times do
+  ingredients = Ingredient.new(name: list_of_ingredients["drinks"].sample["strIngredient1"])
+  if ingredients.save
+    puts ingredients.name + " created!"
+  end
+end
+
+puts "#{Ingredient.count} ingredients created!"
+
+
+puts "Deleting all cocktails"
+Cocktail.destroy_all
+
+20.times do
+  cocktails = Cocktail.new(name: ["cocktail A", "cocktail B", "cocktail C", "cocktail D", "cocktail E"].sample)
+  if cocktails.save
+    puts cocktails.name + " created!"
+  end
+end
