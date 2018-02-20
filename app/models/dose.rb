863da9b5b074af752706a02c745c49009537bf3f
clement bonnemaire <clement.bonnemaire@gmail.com>
2018-02-20 12:26:20 +0800

    prepare for heroku

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,8 +2,8 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
   validates :description, presence: true
-  validates :cocktail_id, presence: true
-  validates :ingredient_id, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
   # validating unique ingredient/cocktail pair
-  validates_uniqueness_of :ingredient_id, :scope => [:cocktail_id]
+  validates :ingredient, uniqueness: { scope: :cocktail }
 end
